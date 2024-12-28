c4ea3e5c5776a9d6b516db4c15ce3211f1565e04
Emma Alexandria <emma.jellemabutler@gmail.com>
2024-12-28 09:41:38 +0200

    Working preferences form

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,5 +1,14 @@
-from flask import Blueprint, Response, jsonify, make_response, request
-from ..db import DB
+from flask import (
+    Blueprint,
+    Response,
+    json,
+    jsonify,
+    make_response,
+    render_template,
+    render_template_string,
+    request,
+)
+from ..db import DB, GenderPreference, Preferences
 
 api_bp = Blueprint("api", __name__)
 
@@ -7,11 +16,36 @@ api_bp = Blueprint("api", __name__)
 def register_routes(db: DB):
     @api_bp.route("/preferences", methods=["GET", "POST"])
     def preferences():
-        if request.method == "GET":
-            return jsonify(db.get_preferences())
         if request.method == "POST":
-            preferences = request.form["preferences"]
-            print(preferences)
-            return make_response(200)
-
+            print(request.form)
+            form = request.form
+            male = False
+            female = False
+            nonbinary = False
+            if form.get("male", False):
+                male = True
+            if form.get("female", False):
+                female = True
+            if form.get("nonbinary", False):
+                nonbinary = True
+            r_preferences = Preferences(
+                age_min=int(float(form["age-min"])),
+                age_max=int(float(form["age-max"])),
+                distance_meters=int(float(form["distanceKms"]) * 1000),
+                gender=GenderPreference(
+                    male,
+                    female,
+                    nonbinary,
+                ),
+            )
+            db.get_current_user().preferences = r_preferences
+            db.save()
+            return render_template_string(
+                """
+                 {% from "includes/macros.html" import preferences with context %}
+            
+                 {{preferences(r_preferences)}}
+            """,
+                r_preferences=r_preferences,
+            )
         return make_response(400)
